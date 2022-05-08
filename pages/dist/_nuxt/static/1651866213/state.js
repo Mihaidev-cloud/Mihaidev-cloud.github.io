a2f90d1f66fdcabdd98ccdcebdec606c595e5a3d
Mihai <69433258+Mihaidev-cloud@users.noreply.github.com>
2022-05-08 21:36:55 +0300

    Add files via upload

---
+++ pages/dist/_nuxt/static/1651866213/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FMihaidev-cloud.github.io\u002F_nuxt\u002Fstatic\u002F1651866213",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FMihaidev-cloud.github.io\u002F",assetsPath:"\u002FMihaidev-cloud.github.io\u002F_nuxt\u002F",cdnURL:a}}}}(null));
